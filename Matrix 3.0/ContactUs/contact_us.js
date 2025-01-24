08a58ec88fa04dad0a8b950abecb02e17cc6740b
Vivek Dutt Sharma <158206172+vivekdsharma@users.noreply.github.com>
2025-01-24 18:21:46 +0530

    Delete Matrix 3.0 directory

--- Matrix 3.0/ContactUs/contact_us.js
+++
@@ -1,7 +0,0 @@
-// JavaScript placeholder
-// Add any interaction logic or validations here
-
-document.querySelector('.query-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    alert('Query submitted successfully!');
-});
